8f839586c17b1cae0adc9eda5eb7ad96baa607fc
tanishthorat <tanishthorat02@gmail.com>
2026-05-26 00:49:32 +0530

    update: sidebar

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -9,7 +9,7 @@ import Logo, { CompactLogo } from "../ui/logo";
 const navigation = [
   { name: "Home", href: "/dashboard", icon: Home },
   { name: "Templates", href: "/dashboard/templates", icon: Mail },
-  { name: "Pre-built", href: "/dashboard/pre-built", icon: LayoutTemplate },
+  { name: "Designs", href: "/dashboard/pre-built", icon: LayoutTemplate },
   //  coming soon in future
   // { name: "Components", href: "/dashboard/components", icon: Palette }, 
   // { name: "Settings", href: "/dashboard/settings", icon: Settings },
